3179e165f11daa325852e6d9a904665c69fc6ecb
OtohaMinori <otohaminori@users.noreply.github.com>
2022-08-17 05:26:10 +0900

    Add ready event

--- Events/Ready/Processing.js
+++ Events/Ready/Processing.js
@@ -1,33 +1,33 @@
 // Color log settings
-const logger = require('node-color-log');
+const logger = require("node-color-log");
 logger.setDate(() => new Date().toLocaleString(undefined, {
-	year:   'numeric',
-	month:  '2-digit',
-	day:    '2-digit',
-	hour:   '2-digit',
+	year:   "numeric",
+	month:  "2-digit",
+	day:    "2-digit",
+	hour:   "2-digit",
 	hour12: false,
-	minute: '2-digit',
-	second: '2-digit',
-}).replace(/\//g, '-'));
-logger.setLevel('info');
+	minute: "2-digit",
+	second: "2-digit",
+}).replace(/\//g, "-"));
+// logger.setLevel('info');
 
 module.exports = {
-	name: 'ready',
+	name: "ready",
 	once: true,
 	execute(client) {
 		client.user.setPresence(
 			{
 				activities: [
 					{
-						name: 'まつりとサラ',
-						type: 'PLAYING',
+						name: "まつりとサラ",
+						type: "PLAYING",
 					}
 				],
-				status:     'online',
+				status:     "online",
 			}
 		);
 		logger.debug(`⏳️Trying to login system with ${client.user.tag}...`);
-		logger.debug('✔️Logged in success!');
+		logger.debug("✔️Logged in success!");
 		logger.info(`Logged in user:${client.user.tag}!`);
 	},
 };
--- bot.js
+++ bot.js
@@ -1,43 +1,47 @@
 // Require modules
-const {Client, GatewayIntentBits} = require('discord.js');
-const dotenv = require('dotenv');
-const fs = require('node:fs');
-const logger = require('node-color-log');
-const path = require('node:path');
+const {Client, GatewayIntentBits} = require("discord.js");
+const dotenv = require("dotenv");
+const fs = require("node:fs");
+const logger = require("node-color-log");
 
 // Color log settings
 logger.setDate(() => new Date().toLocaleString(undefined, {
-	year:   'numeric',
-	month:  '2-digit',
-	day:    '2-digit',
-	hour:   '2-digit',
+	year:   "numeric",
+	month:  "2-digit",
+	day:    "2-digit",
+	hour:   "2-digit",
 	hour12: false,
-	minute: '2-digit',
-	second: '2-digit',
-}).replace(/\//g, '-'));
-logger.setLevel('info');
+	minute: "2-digit",
+	second: "2-digit",
+}).replace(/\//g, "-"));
+// logger.setLevel('info');
 
 // Environment variables
 dotenv.config();
 const token = process.env.TOKEN;
 
 // Define client
-const client = new Client({intents: [GatewayIntentBits.Guilds]});
+const client = new Client(
+	{
+		intents:
+			[
+				GatewayIntentBits.Guilds
+			]
+	}
+);
 
 // TODO:Command Handler
 
 // Event handler
-const eventFolder = fs.readdirSync("./Events")
+const eventFolder = fs.readdirSync("./Events");
 for (const Folder of eventFolder) {
-	logger.debug(Folder)
-	const eventFiles = fs.readdirSync(`./Events/${Folder}`).filter(file => file.endsWith('.js'));
+	const eventFiles = fs.readdirSync(`./Events/${Folder}`).filter((file) => file.endsWith(".js"));
 	for (const Files of eventFiles) {
-		const filePath = path.join("./Events", Folder, Files);
-		logger.debug(filePath);
-		const event = require(filePath);
+		const event = require(`./Events/${Folder}/${Files}`);
 		if (event.once) {
 			client.once(event.name, (...args) => event.execute(...args));
-		} else {
+		}
+		else {
 			client.on(event.name, (...args) => event.execute(...args));
 		}
 	}
